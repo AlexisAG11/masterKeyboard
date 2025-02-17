7dc0414cf7b6811a99504b72010ad9cedbbdd6b4
Alexis <pseudoalex@outlook.fr>
2025-02-17 19:52:32 +0100

    add font and timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { interval, Subscription } from 'rxjs';
 
 
 @Component({
@@ -48,12 +49,17 @@ export class AppComponent implements OnInit {
     //   }, 2000)
     // })
   }
+
+
   isCorrect = false;
   isUncorrect = false;
+  counterWord = 0;
+
   checkWord(){
     if(this.userInput === this.combinedArray[this.indexWord]){
       // good so next word
       this.isCorrect=true;
+      this.counterWord++;
       console.log("well done");
       this.userInput = "";
       if (this.indexWord===30) {
@@ -75,10 +81,56 @@ export class AppComponent implements OnInit {
     }
   }
 
+
+  milliseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  private timerSubscription!: Subscription;
+
+  startTimer(): void {
+    this.timerSubscription = interval(10).subscribe(() => {
+      this.milliseconds += 10;
+      if (this.milliseconds >= 1000) {
+        this.milliseconds = 0;
+        this.seconds++;
+      }
+      if (this.seconds >= 60) {
+        this.seconds = 0;
+        this.minutes++;
+      }
+    });
+  }
+
+  stopTimer(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  resetTimer(): void {
+    this.stopTimer();
+    this.milliseconds = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+  }
+
+  formatTime(value: number): string {
+    // Format time to have 2 digits (e.g., 09 instead of 9)
+    return value < 10 ? '0' + value : value.toString();
+  }
+  formatTimeMili(value: number): string {
+    // Format time to have 2 digits (e.g., 09 instead of 9)
+    let val = value === 0 ? "00" : value < 100 ? "0" + value.toString().slice(0, -1) : value.toString().slice(0, -1);
+    return val
+  }
+
+
   ngOnInit(): void {
     // TO NOT CALL THE API FOR DEV PURPOSE
 
     this.combinedArray = ['cat', 'dog', 'sun', 'book', 'tree', 'car', 'fish', 'star', 'hat', 'cup', 'apple', 'bridge', 'guitar', 'flower', 'bicycle', 'friend', 'window', 'pencil', 'otter', 'mountain', 'acquiesce', 'quintessential', 'ephemeral', 'infrastructure', 'juxtaposition', 'onomatopoeia', 'extrapolate', 'philosophical', 'superfluous', 'ubiquitous'];
   }
+
+
 
 }
